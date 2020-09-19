5d4cb8696504b7ba44dbf47de549ececd56a612a
Leonardo Felipe <leonardo.santos@grupofasitec.com.br>
2020-09-19 10:07:49 -0300

    test/fasitec - Definição da facade de contrato

---
+++ Fasitec.Business/ApplicationServices/IContratoFacade.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using Fasitec.Business.Dto;
+
+namespace Fasitec.Business.ApplicationServices
+{
+    public interface IContratoFacade
+    {
+        IQueryable<ContratoOutput> All();        
+        ContratoOutput ById(int contratoId);
+        ContratoOutput Create(ContratoInput contratoInput);
+        ContratoOutput Modify(UserInput contratoInput);
+        void Remove(int contratoId);
+    }
+}
